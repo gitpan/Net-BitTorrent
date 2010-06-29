3f7601225b14c1b0a4d3d621ad3702315c84bd70
Sanko Robinson <sanko@cpan.org>
2010-06-29 03:24:20 +0000

    Import of SANKO/Net-BitTorrent-0.074000_002 from CPAN.
    gitpan-cpan-distribution: Net-BitTorrent
    gitpan-cpan-version:      0.074000_002
    gitpan-cpan-path:         SANKO/Net-BitTorrent-0.074000_002.tar.gz
    gitpan-cpan-author:       SANKO
    gitpan-cpan-maturity:     developer

--- lib/Net/BitTorrent/Protocol/BEP05/Node.pm
+++ lib/Net/BitTorrent/Protocol/BEP05/Node.pm
@@ -7,7 +7,7 @@ package Net::BitTorrent::Protocol::BEP05::Node;
     use Net::BitTorrent::Types qw[NBTypes::DHT::NodeID];
     use Net::BitTorrent::Protocol::BEP05::Packets qw[:all];
     use 5.10.0;
-    our $MAJOR = 0.074; our $MINOR = 0; our $DEV = 1; our $VERSION = sprintf('%1.3f%03d' . ($DEV ? (($DEV < 0 ? '' : '_') . '%03d') : ('')), $MAJOR, $MINOR, abs $DEV);
+    our $MAJOR = 0.074; our $MINOR = 0; our $DEV = 2; our $VERSION = sprintf('%1.3f%03d' . ($DEV ? (($DEV < 0 ? '' : '_') . '%03d') : ('')), $MAJOR, $MINOR, abs $DEV);
 
     #
     sub BUILD {1}
@@ -94,11 +94,12 @@ package Net::BitTorrent::Protocol::BEP05::Node;
         );
         $code->($self, $tid, $args);
     };
-    has 'ping_timer' => (isa      => 'ArrayRef',
-                         builder  => '_build_ping_timer',
-                         is       => 'ro',
-                         init_arg => undef,
-                         writer   => '_ping_timer'
+    has 'ping_timer' => (
+         isa      => 'Ref',                               # ArrayRef|EV::Timer
+         builder  => '_build_ping_timer',
+         is       => 'ro',
+         init_arg => undef,
+         writer   => '_ping_timer'
     );
 
     sub _build_ping_timer {
@@ -312,3 +313,43 @@ package Net::BitTorrent::Protocol::BEP05::Node;
     );
 }
 1;
+
+=pod
+
+=head1 NAME
+
+Net::BitTorrent::Protocol::BEP05::Node - A single node in a DHT routing table
+
+=head1 Description
+
+TODO
+
+=head1 Author
+
+Sanko Robinson <sanko@cpan.org> - http://sankorobinson.com/
+
+CPAN ID: SANKO
+
+=head1 License and Legal
+
+Copyright (C) 2008-2010 by Sanko Robinson <sanko@cpan.org>
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of
+L<The Artistic License 2.0|http://www.perlfoundation.org/artistic_license_2_0>.
+See the F<LICENSE> file included with this distribution or
+L<notes on the Artistic License 2.0|http://www.perlfoundation.org/artistic_2_0_notes>
+for clarification.
+
+When separated from the distribution, all original POD documentation is
+covered by the
+L<Creative Commons Attribution-Share Alike 3.0 License|http://creativecommons.org/licenses/by-sa/3.0/us/legalcode>.
+See the
+L<clarification of the CCA-SA3.0|http://creativecommons.org/licenses/by-sa/3.0/us/>.
+
+Neither this module nor the L<Author|/Author> is affiliated with BitTorrent,
+Inc.
+
+=for rcs $Id: Notes.pod 5d9d1db 2010-06-07 13:52:01Z sanko@cpan.org $
+
+=cut
